93b71c482fd722fdeafe96a33eb32cd9f878196a
Bailey Moir <bailey.p.moir@gmail.com>
2023-10-19 18:23:51 +1300

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,65 @@
 #include "main.h"
+#include <string.h>
+#include <iostream> 
 
-// Magic functions
-void disabled() {}
-void competition_initialize() {}
-void autonomous() {}
+Motor_Group l_mtrs({12,2,1}),
+			r_mtrs({16,14,3}); 
+Motor_Group cplt_mtrs({Motor(20, false), Motor(10, true)});
 
-void on_center_button() {
-	static bool pressed = false;
-	
-	if (pressed =! pressed) lcd::set_text(2, "I was pressed!");
-	else lcd::clear_line(2);
+double max(double a, double b) {
+	return a > b ? a : b;
 }
+
+void autonomous() {
+	// Note: Use pros::Task	
+	l_mtrs = 64;
+	r_mtrs = 127;
+
+	delay(1000);
+
+	l_mtrs = 0;
+	r_mtrs = 0;
+}
+
+// 60 going negative
 
 void initialize() {
 	lcd::initialize();
-	lcd::set_text(1, "Hello PROS User!");
+	lcd::set_text(1, "Integral Integral");
 
-	lcd::register_btn1_cb(on_center_button);
+	cplt_mtrs.set_encoder_units(E_MOTOR_ENCODER_DEGREES);
+
+	l_mtrs.set_brake_modes(E_MOTOR_BRAKE_HOLD);
+	r_mtrs.set_brake_modes(E_MOTOR_BRAKE_HOLD);
+	r_mtrs.set_reversed(true);
+	cplt_mtrs.set_brake_modes(E_MOTOR_BRAKE_HOLD);
 }
 
 void opcontrol() {
 	Controller master(E_CONTROLLER_MASTER);
 
-	Motor_Group l_mtrs = Motor_Group({1,2,3});
-	Motor_Group r_mtrs = Motor_Group({4,5,6});
-
-	l_mtrs.set_brake_modes(E_MOTOR_BRAKE_HOLD);
-	r_mtrs.set_brake_modes(E_MOTOR_BRAKE_HOLD);
+	const double lwr = 45, upr = 60, k = 1;
 
 	for (;;) {
-		auto btn_vals = lcd::read_buttons();
-		lcd::print(0, "%d %d %d",	(btn_vals & LCD_BTN_LEFT) >> 2,
-		                			(btn_vals & LCD_BTN_CENTER) >> 1,
-		                 			(btn_vals & LCD_BTN_RIGHT) >> 0);
+		const int32_t left = master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y),
+					  right = master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y);
 		
-		int32_t left = master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y),
-				right = master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y);
-		if (!left && !right) {
-			l_mtrs.brake();
-			r_mtrs.brake();
-		} else {
+		if (left || right) {
 			l_mtrs = left;
 			r_mtrs = right;
+		} else {
+			l_mtrs.brake();
+			r_mtrs.brake();
 		}
+		
+		const int32_t sht_fwd = master.get_digital(E_CONTROLLER_DIGITAL_R1),
+					  sht_bck = master.get_digital(E_CONTROLLER_DIGITAL_R2);
+
+		const double cplt_pos = cplt_mtrs.get_positions()[0];
+		if ((cplt_pos > lwr || sht_fwd) && (cplt_pos < upr || sht_bck))
+			cplt_mtrs = 127 * (sht_fwd*(1 - exp(k*(cplt_pos - upr))) - sht_bck*(1 - exp(-k*(cplt_pos - lwr))));
+		else cplt_mtrs.brake();
+		lcd::set_text(2,std::to_string(cplt_pos));
 		
 		delay(20);
 	}
